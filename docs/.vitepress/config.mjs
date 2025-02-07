db855393fb0794d31c5aa1ee0d7705b781942821
zhouzenghui <zhouzenghui@njsdata.com>
2025-02-07 09:53:27 +0800

    完善首页

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,10 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "My Blog",
   description: "个人博客说明",
+  head: [
+    // 配置网站的图标（显示在浏览器的 tab 上）
+    ["link", { rel: "icon", href: "../../public/logo.jpg" }],
+  ],
   themeConfig: {
     logo: "/logo.jpg",
     // https://vitepress.dev/reference/default-theme-config
